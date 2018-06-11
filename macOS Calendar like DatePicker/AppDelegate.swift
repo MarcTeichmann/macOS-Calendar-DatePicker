e05e1c5802bde1e1b6be8e21ca913d532378811b
Marc Teichmann <marc.teichmann@me.com>
2018-06-11 06:49:19 +0200

    Initial Commit

---
+++ macOS Calendar like DatePicker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macOS Calendar like DatePicker
+//
+//  Created by Marc Teichmann on 11.06.18.
+//  Copyright © 2018 Celmaro. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macOS Calendar like DatePicker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macOS Calendar like DatePicker
+//
+//  Created by Marc Teichmann on 11.06.18.
+//  Copyright © 2018 Celmaro. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
