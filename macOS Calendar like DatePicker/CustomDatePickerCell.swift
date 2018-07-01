fe206de564b64c795d83ea24897f953e537b9988
Marc Teichmann <marc.teichmann@me.com>
2018-07-01 10:05:40 +0200

    Custom NSDatePickerCell

---
+++ macOS Calendar like DatePicker/CustomDatePickerCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomDatePickerCell.swift
+//  macOS Calendar like DatePicker
+//
+//  Created by Marc Teichmann on 01.07.18.
+//  Copyright © 2018 Celmaro. All rights reserved.
+//
+
+import Cocoa
+
+class CustomDatePickerCell: NSDatePickerCell {
+
+    override func draw(withFrame cellFrame: NSRect, in controlView: NSView) {
+        super.draw(withFrame: cellFrame, in: controlView)
+        // draw your custom cell
+        //print("Draw")
+    }
+    
+}
